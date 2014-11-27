0ed5dbd0b1217fcf7202fa7b3ce39657d8fd73c7
Richard Pett <rich_pett@hotmail.com>
2014-11-27 16:30:13 +0000

    Completed Object-Oriented Bootcamp

--- example/app/routes.php
+++ example/app/routes.php
@@ -27,7 +27,83 @@ Route::get('/users/{username}', function($username){
 
 });
 
+Route::get('/oop', function(){
 
+	class Task{
+
+		public $title;
+		public $description;
+		public $completed = false;
+
+		public function __construct($title, $description){
+
+			$this->title = $title;
+			$this->description = $description;
+
+		}
+
+		public function complete(){
+
+			$this->completed = true;
+
+		}
+
+	}
+
+	// $task = new Task('Learn OOP', 'Sign up to laracasts and follow along with the videos');
+	// $task->complete();
+	// var_dump($task->completed);
+	// return $task->title . " - " . $task->description . " Completed = " . $task->completed;
+
+	class Person{
+
+		public $name;
+		public $age;
+
+		public function __construct($name){
+
+			$this->name = $name;
+
+		}
+
+		public function setAge($age){
+
+			if( $age < 18){
+
+				echo "Person is too young";
+
+			}else{
+
+				$this->age = $age;
+
+			}
+
+		}
+
+		public function getAge(){
+
+			return ($this->age * 365);
+
+		}
+
+	}
+
+	$rich = new Person('Richard Pett');
+
+	// Manipulating the propterty directly
+	// $rich->age = 12;
+
+	// We should use setters so that we can set rules about how the property should be set
+	$rich->setAge(19);
+
+	// We can also use getters to specify behavior when we get the age. E.g. return the age in number of days	
+	var_dump($rich->getAge());
+
+
+	// var_dump($rich);
+
+
+});
 
 // Example of basic crud operations
 // Route::get('/', function(){
